c265acc9ad63325382613800b0686552b7713f7b
Michael Navazhylau <mikipux7@gmail.com>
2017-05-16 19:45:11 -0500

    added speech agent

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ function outputMessage(message) {
   $("#output").text(message);
 }
 
+function speekMessage(message) {
+  if ('speechSynthesis' in window) {
+    var msg = new SpeechSynthesisUtterance(message);
+    window.speechSynthesis.speak(msg);
+  }else {
+    console.log("unable to use speech synth");
+  }
+
+}
+
 
 $(document).ready(function () {
 
@@ -9,13 +19,14 @@ $(document).ready(function () {
     e.preventDefault();
 
     var inputstring = $("#searchInput").val();
-    
+
     $.ajax({
       type: "POST",
       url: "./brain.php",
       data: {"input": inputstring},
     }).done (function (data) {
       outputMessage(data);
+      speekMessage(data);
     })
   });
 
