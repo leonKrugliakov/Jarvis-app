4b632886ac6eecb7b135a9aa75f6be8dcbbbdb91
leonKrugliakov <leonchikyj@gmail.com>
2017-05-16 20:25:04 -0500

    attempt at a different translate api

--- brain.php
+++ brain.php
@@ -124,37 +124,12 @@
 
     echo $langTo . "<br /><br />";
 
-    echo "
-      <script>
-        const translate = require('google-translate-api');
+    $languages = "http://www.transltr.org/api/getlanguagesfortranslate";
+    $languages = file_get_contents($languages);
+    $languages = json_decode($languages);
 
-        translate('Ik spreek Engels', {to: 'en'}).then(res => {
-          console.log(res.text);
-          //=> I speak English
-          console.log(res.from.language.iso);
-          //=> nl
-        }).catch(err => {
-          console.error(err);
-        });
+    echo $languages;
 
-
-        translate('I spea Dutch!', {from: 'en', to: 'nl'}).then(res => {
-            console.log(res.text);
-            //=> Ik spreek Nederlands!
-            console.log(res.from.text.autoCorrected);
-            //=> true
-            console.log(res.from.text.value);
-            //=> I [speak] Dutch!
-            console.log(res.from.text.didYouMean);
-            //=> false
-        }).catch(err => {
-            console.error(err);
-        });
-      </script>
-
-
-
-    ";
 
 
   }
