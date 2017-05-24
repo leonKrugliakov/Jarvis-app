9fd87eefbd09534051f81e1b1c35b6ce72d669bf
Michael Navazhylau <mikipux7@gmail.com>
2017-05-24 15:33:34 -0500

    completed base location code

--- location.php
+++ location.php
@@ -1,8 +1,16 @@
 <?php
 
+function getKeys() {
+  $keys_data = file_get_contents("./keys.json");
+  $keys = json_decode($keys_data);
+
+  return $keys;
+}
+
+
+
 function findPerson($location) {
-  $json = file_get_contents("./keys.json");
-  $keys = json_decode($json);
+  $keys = getKeys();
   $googlekey = $keys->googlemaps;
   $request = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='. $location[latitude] .','. $location[longitude] .'&sensor=false&location_type=ROOFTOP&result_type=street_address&key='. $keys->googlemaps;
 
@@ -12,12 +20,28 @@ function findPerson($location) {
 
 }
 
+//TODO finish loco
+
 function findLocation($input) {
+
+  $keys = getKeys();
+
   $startphrase = "where is ";
   $start = strpos($input, $startphrase) + strlen($startphrase);
-  $item = substr($input, $start, strlen($input));
+  $address = substr($input, $start, strlen($input));
 
-  echo $item;
+  $request = 'https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($address) . '&key=' . $keys->googlemaps;
+
+  $loco_data = file_get_contents($request);
+  $parsed_loco = json_decode($loco_data);
+  $geometry = $parsed_loco->results[0]->geometry;
+
+  $latitude = $geometry->location->lat;
+  $longitude = $geometry->location->lng;
+
+  $message = "You are located at " . $latitude . " latitude and " . $longitude . " longitude.";
+
+  echo $message;
 
 }
 
